d962423b9393b9cae14f1d0262cb244502579757
rymmsb-beep <rymmsb290@gmail.com>
2026-06-07 20:15:59 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,29 +1,29 @@
 const products = [
 
-  {id:1,name:"Ball Track Kmart",price:49.90,category:"Fun Toys Interactive Games",img:"../image/ft1.jpg"},
-  {id:2,name:"Puzzle Cat Toy",price:59.90,category:"Fun Toys Interactive Games",img:"../image/ft2.jpg"},
-  {id:3,name:"Automatic Toy",price:79.90,category:"Fun Toys Interactive Games",img:"../image/ft3.jpg"},
+  {id:1,name:"Ball Track Kmart",price:49.90,category:"Fun Toys Interactive Games",img:"image/ft1.jpg"},
+  {id:2,name:"Puzzle Cat Toy",price:59.90,category:"Fun Toys Interactive Games",img:"image/ft2.jpg"},
+  {id:3,name:"Automatic Toy",price:79.90,category:"Fun Toys Interactive Games",img:"image/ft3.jpg"},
 
 
-  {id:11,name:"Collar with Bell",price:15.90,category:"Collars Items",img:"../image/ci1.jpg"},
-  {id:12,name:"Pet Faux Pearl Necklace",price:35.90,category:"Collars Items",img:"../image/ci2.jpg"},
-  {id:13,name:"Petface Accessories",price:12.90,category:"Collars Items",img:"../image/ci3.jpg"},
-  {id:14,name:"Harnesses",price:49.90,category:"Collars Items",img:"../image/ci4.jpg"},
+  {id:11,name:"Collar with Bell",price:15.90,category:"Collars Items",img:"image/ci1.jpg"},
+  {id:12,name:"Pet Faux Pearl Necklace",price:35.90,category:"Collars Items",img:"image/ci2.jpg"},
+  {id:13,name:"Petface Accessories",price:12.90,category:"Collars Items",img:"image/ci3.jpg"},
+  {id:14,name:"Harnesses",price:49.90,category:"Collars Items",img:"image/ci4.jpg"},
 
-  {id:21,name:"Soft Cushion",price:24.90,category:"Beds & Cushions",img:"../image/bed1.jpg"},
-  {id:22,name:"Round Bed",price:29.90,category:"Beds & Cushions",img:"../image/bed2.jpg"},
-  {id:23,name:"Premium Bed",price:49.90,category:"Beds & Cushions",img:"../image/bed3.jpg"},
-  {id:24,name:"XXL Cushion",price:34.90,category:"Beds & Cushions",img:"../image/bed4.jpg"},
+  {id:21,name:"Soft Cushion",price:24.90,category:"Beds & Cushions",img:"image/bed1.jpg"},
+  {id:22,name:"Round Bed",price:29.90,category:"Beds & Cushions",img:"image/bed2.jpg"},
+  {id:23,name:"Premium Bed",price:49.90,category:"Beds & Cushions",img:"image/bed3.jpg"},
+  {id:24,name:"XXL Cushion",price:34.90,category:"Beds & Cushions",img:"image/bed4.jpg"},
 
-  {id:31,name:"Dental Hygiene Set",price:19.90,category:"Care & Hygiene",img:"../image/ch1.jpg"},
-  {id:32,name:"Cordless Cat Trimmer",price:24.90,category:"Care & Hygiene",img:"../image/ch2.jpg"},
-  {id:33,name:"Health Care Kit",price:34.90,category:"Care & Hygiene",img:"../image/ch3.jpg"},
-  {id:34,name:"Marltons Pet Care",price:39.90,category:"Care & Hygiene",img:"../image/ch4.jpg"},
+  {id:31,name:"Dental Hygiene Set",price:19.90,category:"Care & Hygiene",img:"image/ch1.jpg"},
+  {id:32,name:"Cordless Cat Trimmer",price:24.90,category:"Care & Hygiene",img:"image/ch2.jpg"},
+  {id:33,name:"Health Care Kit",price:34.90,category:"Care & Hygiene",img:"image/ch3.jpg"},
+  {id:34,name:"Marltons Pet Care",price:39.90,category:"Care & Hygiene",img:"image/ch4.jpg"},
 
-  {id:41,name:"Healthy Gourmet",price:90,category:"Healthy Cat Food",img:"../image/f1.jpg"},
-  {id:42,name:"Flash Sales Food",price:100,category:"Healthy Cat Food",img:"../image/f2.jpg"},
-  {id:43,name:"Fresh Chicken",price:76,category:"Healthy Cat Food",img:"../image/f3.jpg"},
-  {id:44,name:"Tuna & Chicken",price:85,category:"Healthy Cat Food",img:"../image/f4.jpg"}
+  {id:41,name:"Healthy Gourmet",price:90,category:"Healthy Cat Food",img:"image/f1.jpg"},
+  {id:42,name:"Flash Sales Food",price:100,category:"Healthy Cat Food",img:"image/f2.jpg"},
+  {id:43,name:"Fresh Chicken",price:76,category:"Healthy Cat Food",img:"image/f3.jpg"},
+  {id:44,name:"Tuna & Chicken",price:85,category:"Healthy Cat Food",img:"image/f4.jpg"}
 
 ];
 
